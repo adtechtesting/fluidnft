defac91941e3a229480992d9ce6d5a9d84fd59b1
tester <webd78178@gmail.com>
2025-04-06 00:05:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- 
+ reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.alias['@metaplex-foundation/mpl-candy-machine'] = false;
+    return config;
+  },
   
 };
 
