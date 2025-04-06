2b84ebe699c1ed70e58efaad64512c461407502d
tester <webd78178@gmail.com>
2025-04-06 23:09:10 +0530

    Update and rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
- reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.resolve.alias['@metaplex-foundation/mpl-candy-machine'] = false;
     return config;
   },
-  
 };
 
 export default nextConfig;
